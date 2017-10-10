a6c1e0da699368899ef36bcaacf4741e69f84ed6
Mike Heft <mikeheft@gmail.com>
2017-10-10 07:09:36 -0600

    Add github omniauth gem, routes and create action

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def create
+    if params[:provider].present?
+      @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+      set_session_user_id
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+
+  def set_session_user_id
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "Logged in as #{@user.name}"
+      redirect_to root_path
+    end
+  end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'auth/:provider/callback',  to: 'sessions#create'
+  get 'auth/failure',             to: redirect('/')
+  get 'signout',                  to: 'sessions#destroy', as: 'signout'
+
+end
