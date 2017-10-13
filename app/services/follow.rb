a561ec4e7e6b9d44b3cbda5af30a0dad23ecf10c
Mike Heft <mikeheft@gmail.com>
2017-10-12 20:13:40 -0600

    Add total commits for followers and following

--- app/services/follow.rb
+++ app/services/follow.rb
@@ -1,20 +1,41 @@
+require 'date'
+
 class Follow
 
-  attr_reader :name, :image_url, :html_url
+  attr_reader :name, :image_url, :html_url, :uid
 
   def initialize(repo)
     @name = repo[:login]
     @image_url = repo[:avatar_url]
     @html_url = repo[:html_url]
+    @uid       = repo[:id]
+    api_call
   end
 
-  def commits
+  def one_week_ago
+    (DateTime.now - 7).to_s
+  end
+
+  def api_call
     @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.authorization :token, "725ba0ecf339d46d8b518dcfc52f13e74ca45b84"
+      faraday.headers['If-Modified-Since'] == one_week_ago
       faraday.headers["Accept"] = 'application/vnd.github.cloak-preview+json'
       faraday.headers["X-API-KEY"] = ENV["GITHUB_KEY"]
       faraday.adapter Faraday.default_adapter
     end
-    response = @conn.get("/search/commits?q=author:#{name}")
-    JSON.parse(response.body)['total_count']
+    # response = @conn.get("/search/commits?q=author:#{name}&perpage=100")
+    # require 'pry'; binding.pry
+    # JSON.parse(response.body)
+  end
+
+
+  def get_url(url)
+    response = @conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def commits
+    get_url("/search/commits?q=author:#{name}")[:total_count]
   end
 end
